44f9d4a760621889d673dd096b845b0fa816116f
Marius <mariusbogdan83@gmail.com>
2015-02-11 18:22:14 +0100

    updated Imagethumb and added tests

--- src/Kisphp/ImageResizer.php
+++ src/Kisphp/ImageResizer.php
@@ -1,28 +1,50 @@
 <?php
 
-namespace Kisphp\Images;
+namespace Kisphp;
+
+use Kisphp\Helper;
 
 class ImageResizer extends Helper
 {
-    public      $quality    = 85;
-    public      $mime       = '';
-    public      $thumb      = '';
-    public      $target     = '';
-    protected   $src_w      = 0;
-    protected   $src_h      = 0;
-    private     $orig_w     = 0;
-    private     $orig_h     = 0;
-    private     $new_w      = 0;
-    private     $new_h      = 0;
-    
+    public $quality = 85;
+    public $mime    = '';
+    public $thumb   = '';
+    public $target  = '';
+
+    protected $sourceWidth  = 0;
+    protected $sourceHeight = 0;
+
+    private $originalWidth  = 0;
+    private $originalHeight = 0;
+    private $newWidth       = 0;
+    private $newHeight      = 0;
+
+    // default white
+    protected $backgroundColor = array(255, 255, 255);
+
     /**
-     * set the quality of the resulted thumbnail
+     * set the quality of the resulted thumbnail (for jpeg files)
      */
-    public function __construct($quality=85)
+    public function __construct($quality = 85)
     {
         $this->quality = max(1, min(100, abs(intval($quality))));
     }
-    
+
+    /**
+     * @param integer $R
+     * @param integer $G
+     * @param integer $B
+     */
+    public function setBackgroundColor($R, $G, $B)
+    {
+        // make sure that value is between 0 and 255
+        $this->backgroundColor = array(
+            min(255, max(0, (int) $R)),
+            min(255, max(0, (int) $G)),
+            min(255, max(0, (int) $B)),
+        );
+    }
+
     /**
      * load file to manipulate
      * @file_location = /path/to/my/file
@@ -30,48 +52,67 @@ class ImageResizer extends Helper
     public function load($file_location)
     {
         $mime = strtoupper(preg_replace("/.*\.(.*)$/", "\\1", $file_location));
+
         $this->mime = $mime;
         switch ($this->mime) {
+            
             case "PNG":
                 $this->thumb = imagecreatefrompng($file_location);
                 imagealphablending($this->thumb, false); // setting alpha blending off
                 imagesavealpha($this->thumb, true); // save alphablending setting (important)
                 break;
+            
             case "GIF":
                 $this->thumb = imagecreatefromgif($file_location);
                 break;
+            
             case "JPG":
             case "JPEG":
                 $this->thumb = imagecreatefromjpeg($file_location);
                 break;
+            
             default:
                 //die('Not allowed file');
                 break;
         }
-        $this->src_w = $this->new_w = imagesx($this->thumb);
-        $this->src_h = $this->new_h = imagesy($this->thumb);
+        $this->sourceWidth = $this->newWidth = imagesx($this->thumb);
+        $this->sourceHeight = $this->newHeight = imagesy($this->thumb);
     }
-    
+
     /**
      * set the wirth of the thumbnail and calculate the height
      */
-    public function setW($w, $resample=true)
+    protected function setWidth($w, $resample = true)
     {
-        $this->new_w = abs(intval($w));
-        $this->new_h = floor(($w / $this->src_w) * $this->src_h);
-        if ($resample === true) $this->resample(); 
+        $this->newWidth = abs(intval($w));
+        $this->newHeight = floor(($w / $this->sourceWidth) * $this->sourceHeight);
+        if ( $resample === true ) {
+            $this->resample();
+        }
     }
-    
+
     /**
      * set the height of the thumbnail and calculate the witdh
      */
-    public function setH($h, $resample=true)
+    protected function setHeight($h, $resample = true)
     {
-        $this->new_h = abs(intval($h));
-        $this->new_w = floor(($h / $this->src_h) * $this->src_w);
-        if ($resample === true) $this->resample(); 
+        $this->newHeight = abs(intval($h));
+        $this->newWidth = floor(($h / $this->sourceHeight) * $this->sourceWidth);
+        if ( $resample === true ) {
+            $this->resample();
+        }
     }
-    
+
+    public function getFinalWidth()
+    {
+        return $this->newWidth;
+    }
+
+    public function getFinalHeight()
+    {
+        return $this->newHeight;
+    }
+
     /**
      * resample the image
      */
@@ -87,123 +128,117 @@ class ImageResizer extends Helper
             0, // (dst_y) y-coordinate of destination point.
             0, // (src_x) x-coordinate of source point.
             0, // (src_y) y-coordinate of source point.
-            $this->new_w, // (dst_w) Destination width.
-            $this->new_h, // (dst_h) Destination height.
-            $this->src_w, // (src_w) Source width.
-            $this->src_h // (src_h) Source height.
+            $this->newWidth, // (dst_w) Destination width.
+            $this->newHeight, // (dst_h) Destination height.
+            $this->sourceWidth, // (sourceWidth) Source width.
+            $this->sourceHeight // (sourceHeight) Source height.
         );
+        unset($tmp);
     }
-    
-    private function crop($w,$h, $cut_from_image = false)
+
+    private function crop($w, $h, $cut_from_image = false)
     {
         if ( $cut_from_image === true ) {
-            $this->do_simple_crop($w, $h);
+            $this->doSimpleCrop($w, $h);
         } else {
-            if ( $this->src_w >= $this->src_h ) {
-                if ( ($this->src_w / $this->src_h) > ($w / $h) )
-                {
-                    ( $cut_from_image === true ) ? $this->setH($h, false) : $this->setW($w, false);
-                } 
-                else 
-                {
-                    ( $cut_from_image === true ) ? $this->setW($w, false) : $this->setH($h, false);
+            if ( $this->sourceWidth >= $this->sourceHeight ) {
+                if ( ($this->sourceWidth / $this->sourceHeight) > ($w / $h) ) {
+                    ( $cut_from_image === true ) ? $this->setHeight($h, false) : $this->setWidth($w, false);
+                } else {
+                    ( $cut_from_image === true ) ? $this->setWidth($w, false) : $this->setHeight($h, false);
                 }
-            } 
-            else 
-            {
-                if ( $w >= $h )
-                {
-                    ( $cut_from_image === true ) ? $this->setW($w) : $this->setH($h, false);
-                } 
-                else 
-                {
-                    ( $cut_from_image === true ) ? $this->setH($h) : $this->setW($w, false);
+            } else {
+                if ( $w >= $h ) {
+                    ( $cut_from_image === true ) ? $this->setWidth($w) : $this->setHeight($h, false);
+                } else {
+                    ( $cut_from_image === true ) ? $this->setHeight($h) : $this->setWidth($w, false);
                 }
             }
             $this->resample();
-            $this->resample_crop();
+            $this->resampleCrop();
         }
     }
-    
-    private function do_simple_crop($w, $h)
+
+    private function doSimpleCrop($w, $h)
     {
-        if ( $this->src_w >= $this->src_h ) {
-            if ( ($this->src_w / $this->src_h ) > ($w / $h)) {
-                $this->setH($h, false);
+        if ( $this->sourceWidth >= $this->sourceHeight ) {
+            if ( ($this->sourceWidth / $this->sourceHeight) > ($w / $h) ) {
+                $this->setHeight($h, false);
             } else {
-                $this->setW($w, false);
+                $this->setWidth($w, false);
             }
         } else {
             if ( $w >= $h ) {
-                $this->setW($w, false);
+                $this->setWidth($w, false);
             } else {
-                $this->setH($h, false);
+                $this->setHeight($h, false);
             }
         }
         $this->resample();
-        
-        $this->src_w = $this->new_w;
-        $this->src_h = $this->new_h;
+
+        $this->sourceWidth = $this->newWidth;
+        $this->sourceHeight = $this->newHeight;
 
         $this->dst_x = 0;
         $this->dst_y = 0;
         $this->src_x = 0;
         $this->src_y = 0;
-        if ( $this->src_w >= $this->src_h ) {
-            if ( $this->src_w / $this->src_h > $w / $h ) {
-                $this->new_w = $w;
-                $this->dst_x = ($this->src_w - $w) / 2;
+
+        if ( $this->sourceWidth >= $this->sourceHeight ) {
+            if ( $this->sourceWidth / $this->sourceHeight > $w / $h ) {
+                $this->newWidth = $w;
+                $this->dst_x = ($this->sourceWidth - $w) / 2;
             } else {
-                $this->new_h = $h;
-                $this->dst_y = ($this->src_h - $h) / 2;
+                $this->newHeight = $h;
+                $this->dst_y = ($this->sourceHeight - $h) / 2;
             }
         } else {
             if ( $w >= $h ) {
-                $this->new_h = $h;
-                $this->dst_y = ($this->src_h - $h) / 2;
+                $this->newHeight = $h;
+                $this->dst_y = ($this->sourceHeight - $h) / 2;
             } else {
-                $this->new_w = $w;
-                $this->dst_x = ($this->src_w - $w) / 2;
+                $this->newWidth = $w;
+                $this->dst_x = ($this->sourceWidth - $w) / 2;
             }
         }
-        
+
         $tmp = $this->thumb;
         $this->thumb = $this->new_thumb();
-        
+
         imagecopy($this->thumb, // (dst_img) Destination image link resource.
             $tmp, // (src_img) Source image link resource.
             $this->src_x, // (src_x) x-coordinate of source point.
             $this->src_y, // (src_y) y-coordinate of source point.
             $this->dst_x, // (src_x) x-coordinate of source point.
             $this->dst_y, // (src_y) y-coordinate of source point.
-            $this->src_w, // (src_w) Source width.
-            $this->src_h// (src_h) Source height.
+            $this->sourceWidth, // (sourceWidth) Source width.
+            $this->sourceHeight// (sourceHeight) Source height.
         );
     }
-    
+
     /**
      * resize the image and if it is below the requested dimensions puts a blank image below and merges them
      */
-    private function resample_crop()
+    private function resampleCrop()
     {
-        if ( $this->orig_w > 0
-            && $this->orig_h > 0
+        if ( $this->originalWidth > 0
+            && $this->originalHeight > 0
             && (
-                $this->new_w != $this->orig_w
-                || $this->new_h != $this->orig_h
+                $this->newWidth != $this->originalWidth
+                || $this->newHeight != $this->originalHeight
             )
         ) {
             $tmp = $this->thumb;
-            $this->thumb = $this->new_thumb($this->orig_w, $this->orig_h);
-            
+            $this->thumb = $this->new_thumb($this->originalWidth, $this->originalHeight);
+
             $_top = 0;
             $_bottom = 0;
-            
-            if ( $this->new_w != $this->orig_w ) {
-                $_top = ($this->orig_w - $this->new_w) / 2;
+
+            if ( $this->newWidth != $this->originalWidth ) {
+                $_top = ($this->originalWidth - $this->newWidth) / 2;
             }
-            if ( $this->new_h != $this->orig_h ) {
-                $_bottom = ($this->orig_h - $this->new_h) / 2;
+            if ( $this->newHeight != $this->originalHeight ) {
+                $_bottom = ($this->originalHeight - $this->newHeight) / 2;
             }
             imagecopy(
                 $this->thumb,
@@ -212,91 +247,106 @@ class ImageResizer extends Helper
                 $_bottom,
                 0,
                 0,
-                $this->new_w,
-                $this->new_h
+                $this->newWidth,
+                $this->newHeight
             );
         }
     }
-    
+
     /**
      * generate a new thumbnail with the specified dimensions
      * $w = width
      * $h = height
      */
-    public function new_thumb($w=0, $h=0)
+    public function new_thumb($w = 0, $h = 0)
     {
-        $_w = ($w > 0) ? $w : $this->new_w;
-        $_h = ($h > 0) ? $h : $this->new_h;
+        $_w = ($w > 0) ? $w : $this->newWidth;
+        $_h = ($h > 0) ? $h : $this->newHeight;
         if ( $this->mime == 'PNG' ) {
             $this->thumb = imagecreatetruecolor($_w, $_h);
-            $color = imagecolorallocate($this->thumb, 255, 255, 255);
+            $color = imagecolorallocate(
+                $this->thumb,
+                $this->backgroundColor[0],
+                $this->backgroundColor[1],
+                $this->backgroundColor[2]
+            );
             imagefill($this->thumb, 0, 0, $color);
-            imagealphablending($this->thumb, false); // setting alpha blending off
-            imagesavealpha($this->thumb, true); // save alphablending setting (important)
+            // setting alpha blending off
+            imagealphablending($this->thumb, false);
+            // save alphablending setting (important)
+            imagesavealpha($this->thumb, true);
         } else {
             $this->thumb = imagecreatetruecolor($_w, $_h);
-            $color = imagecolorallocate($this->thumb, 255, 255, 255);
+            $color = imagecolorallocate(
+                $this->thumb,
+                $this->backgroundColor[0],
+                $this->backgroundColor[1],
+                $this->backgroundColor[2]
+            );
             imagefill($this->thumb, 10, 10, $color);
-            
+
         }
-        if ( ! isset($this->mime) || $this->mime == '' ) {
+        if ( !isset($this->mime) || $this->mime == '' ) {
             $this->mime = 'JPG';
-            $this->new_w = $_w;
-            $this->new_h = $_h;
+            $this->newWidth = $_w;
+            $this->newHeight = $_h;
         }
         return $this->thumb;
     }
-    
+
     /**
      * this is a controller that decide how to run your code
      * $_w = width
      * $_h = height
      * $cut_from_image = used in crop files if you want to cut from it and center the thumbnail
      */
-    public function resize($_w=0, $_h=0, $cut_from_image = false)
+    public function resize($_w = 0, $_h = 0, $cut_from_image = false)
     {
-        $this->orig_w = $_w;
-        $this->orig_h = $_h;
+        $this->originalWidth = $_w;
+        $this->originalHeight = $_h;
         if ( $_w > 0 && $_h > 0 ) {
             $this->crop($_w, $_h, $cut_from_image);
         } elseif ( $_w > 0 && $_h <= 0 ) {
-            $this->setW($_w, true);
+            $this->setWidth($_w, true);
         } elseif ( $_h > 0 && $_w <= 0 ) {
-            $this->setH($_h, true);
-        } else {
-            
+            $this->setHeight($_h, true);
         }
     }
-    
+
     /**
      * save the file to disk
      */
     public function save()
     {
         switch ($this->mime) {
+
             case "PNG":
                 imagepng($this->thumb, $this->target, 0);
                 break;
+
             case "GIF":
                 imagegif($this->thumb, $this->target);
                 break;
+
             case "JPG":
             case "JPEG":
                 imagejpeg($this->thumb, $this->target, $this->quality);
                 break;
+
             default:
                 die('Not allowed file');
                 break;
         }
     }
-    
+
     /**
      * display the image and save the file to disk (optional)
      */
-    public function display($save=false)
+    public function display($save = false)
     {
         //printr($this); die();
         switch ($this->mime) {
+
             case "PNG":
                 header("Content-type: image/png");
                 if ( $save === true ) {
@@ -304,6 +354,7 @@ class ImageResizer extends Helper
                 }
                 imagepng($this->thumb, NULL, 1);
                 break;
+
             case "GIF":
                 header("Content-type: image/gif");
                 if ( $save === true ) {
@@ -311,6 +362,7 @@ class ImageResizer extends Helper
                 }
                 imagegif($this->thumb, NULL);
                 break;
+
             case "JPG":
             case "JPEG":
                 header("Content-type: image/jpeg");
@@ -319,12 +371,13 @@ class ImageResizer extends Helper
                 }
                 imagejpeg($this->thumb, NULL, $this->quality);
                 break;
+
             default:
                 die('Not allowed file');
                 break;
         }
     }
-    
+
     /**
      * target file where will be the image saved
      */
@@ -332,7 +385,7 @@ class ImageResizer extends Helper
     {
         $this->target = $file_location;
     }
-    
+
     /**
      * return the image string so you will be able to save it into a file
      */
@@ -340,24 +393,30 @@ class ImageResizer extends Helper
     {
         ob_start();
         switch ($this->mime) {
+
             case "PNG":
                 imagepng($this->thumb, NULL, 1);
                 break;
+
             case "GIF":
                 imagegif($this->thumb, NULL);
                 break;
+
             case "JPG":
             case "JPEG":
                 imagejpeg($this->thumb, NULL, $this->quality);
                 break;
+
             default:
-                
+
                 break;
         }
+
         $tmp = ob_get_clean();
         if ( is_resource($this->thumb) ) {
             imagedestroy($this->thumb);
         }
+
         return $tmp;
     }
 }
---
+++ tests/Thumbs/ThumbsTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use Kisphp\ImageResizer;
+
+class ExampleClassTest extends PHPUnit_Framework_TestCase
+{
+    const IMAGE_FILE = '/tmp/kisphp_image_file.jpg';
+
+    public function setUp()
+    {
+        $testImage = imagecreate(200, 80);
+        imagejpeg($testImage, self::IMAGE_FILE);
+    }
+
+    public function tearDown()
+    {
+        if ( is_file(self::IMAGE_FILE) ) {
+            unlink(self::IMAGE_FILE);
+        }
+    }
+
+    public function test_image_width_and_height()
+    {
+        $img = new ImageResizer();
+        $img->load(self::IMAGE_FILE);
+        $img->resize(100, 100);
+
+        $this->assertEquals(100, $img->getFinalWidth());
+        $this->assertNotEquals(100, $img->getFinalHeight());
+    }
+
+    public function test_image_width_and_height_crop()
+    {
+        $img = new ImageResizer();
+        $img->load(self::IMAGE_FILE);
+        $img->resize(100, 100, true);
+
+        $this->assertEquals(100, $img->getFinalWidth());
+        $this->assertEquals(100, $img->getFinalHeight());
+    }
+
+    public function test_image_width()
+    {
+        $img = new ImageResizer();
+        $img->load(self::IMAGE_FILE);
+        $img->resize(100, 0);
+
+        $this->assertNotEquals(0, $img->getFinalHeight());
+    }
+
+    public function test_image_height()
+    {
+        $img = new ImageResizer();
+        $img->load(self::IMAGE_FILE);
+        $img->resize(0, 100);
+
+        $this->assertNotEquals(0, $img->getFinalWidth());
+    }
+}
