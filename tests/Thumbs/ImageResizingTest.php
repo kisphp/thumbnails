d15ddecc44f226484c6789e101313e9b32a092b6
kisphp <mariusbogdan83@gmail.com>
2016-05-12 07:43:45 +0000

    increase tests

---
+++ tests/Thumbs/ImageResizingTest.php
@@ -0,0 +1,53 @@
+<?php
+
+
+class ImageResizingTest extends PHPUnit_Framework_TestCase
+{
+    const IMAGES_DIR = '/../images';
+
+    /**
+     * @param string $source
+     * @param string $target
+     * @param int $width
+     * @param int $height
+     * @throws \Kisphp\ImageFileTypeNotAllowed
+     */
+    protected function resizeImage($source, $target, $width, $height)
+    {
+        $img = new \Kisphp\ImageResizer();
+        $img->load($source);
+        $img->setTarget($target);
+        $img->resize($width, $height);
+        $img->save();
+    }
+
+    /**
+     * @dataProvider fixedResizeProvider()
+     *
+     * @param int $sourceWdith
+     * @param int $sourceHeight
+     * @param int $targetWdith
+     * @param int $targetHeight
+     */
+    public function testResizing($sourceWdith, $sourceHeight, $targetWdith, $targetHeight)
+    {
+        $source = __DIR__ . self::IMAGES_DIR . '/' . $sourceWdith . 'x' . $sourceHeight . '.jpg';
+        $target = __DIR__ . self::IMAGES_DIR . '/th-' . $targetWdith . 'x' . $targetHeight . '.jpg';
+
+        $this->resizeImage($source, $target, $targetWdith, $targetHeight);
+
+        $size = getimagesize($target);
+
+        $this->assertEquals($targetWdith, $size[0]);
+        $this->assertEquals($targetHeight, $size[1]);
+
+        unlink($target);
+    }
+
+    public function fixedResizeProvider()
+    {
+        return [
+            [300, 200, 200, 100],
+        ];
+    }
+}
